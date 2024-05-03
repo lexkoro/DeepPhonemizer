7618f72484f710f98e1e1908bb8b1289e4655aeb
Alexander Korolev <alexx.korolev@gmail.com>
2024-05-03 20:12:02 +0200

    update

--- run_prediction.py
+++ run_prediction.py
@@ -1,19 +1,34 @@
 from dp.phonemizer import Phonemizer
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    checkpoint_path = 'checkpoints/best_model_no_optim.pt'
+    checkpoint_path = "checkpoints/best_model_no_optim.pt"
     phonemizer = Phonemizer.from_checkpoint(checkpoint_path)
 
-    text = 'young'
+    sentences = [
+        "Aus einem fernen Land, von dem du wahrscheinlich noch nie etwas gehört hast, Marvin.",
+        "Moment, woher kennst du meinen Namen?",
+        "Ich bin eine gute Zuhörerin und hier unter Deck ist es nicht gerade geräumig.",
+        "Du scheinst ein netter Junge zu sein. Ich habe ein Angebot für dich.",
+        "Siehst du den Mistkerl, der die Pfeile schnitzt? Dieses Arschloch hat meine Schatulle genommen und sie bei seiner Beute versteckt. Ich habe gesehen, wie er damit im Frachtraum verschwunden ist.",
+        "Ich dachte, er würde meine Schatulle in einer Truhe verstecken, aber dann konnte ich das laute Knarzen von Schiffsplanken hören. Könntest du dich im Frachtraum mal umsehen und schauen, ob es dort ein Versteck gibt?",
+        "Ich habe deine Schatulle gefunden.",
+        "Das ist ja grossartig! Vielen Dank, mein Junge!",
+        "Im Moment habe ich leider keine Möglichkeit dich für deine Hilfe zu bezahlen. Aber ich verspreche dir, sobald wir auf Archolos sind sorge ich dafür, dass meine Freunde dich entlohnen!"
+        "Sieht aus, als wären wir bald da.",
+        "Woher willst du das wissen?",
+        "WAS SOLL DAS?",
+        "Das spiel heisst Gothic.",
+    ]
 
-    result = phonemizer.phonemise_list([text], lang='en_us')
+    for text in sentences:
+        result = phonemizer.phonemise_list([text], lang="de")
+        print(result.predictions)
+        print(result.phonemes)
 
-    print(result.phonemes)
-    for text, pred in result.predictions.items():
-        tokens, probs = pred.phoneme_tokens, pred.token_probs
-        for o, p in zip(tokens, probs):
-            print(f'{o} {p}')
-        tokens = ''.join(tokens)
-        print(f'{text} | {tokens} | {pred.confidence}')
-
+    # for text, pred in result.predictions.items():
+    #     tokens, probs = pred.phoneme_tokens, pred.token_probs
+    #     for o, p in zip(tokens, probs):
+    #         print(f"{o} {p}")
+    #     tokens = "".join(tokens)
+    #     print(f"{text} | {tokens} | {pred.confidence}")
--- run_training.py
+++ run_training.py
@@ -2,30 +2,68 @@ import logging.config
 from pathlib import Path
 import torch
 import torch.multiprocessing as mp
-
+from glob import glob
 from dp.preprocess import preprocess
 from dp.train import train
 from dp.utils.io import read_config
 
-config_file_path = Path('logging.yaml')
+import random
+from collections import defaultdict
+
+config_file_path = Path("logging.yaml")
 config = read_config(config_file_path)
 logging.config.dictConfig(config)
 
-if __name__ == '__main__':
 
-    train_data = [('en_us', 'young', 'jʌŋ'),
-                  ('de', 'benützten', 'bənʏt͡stn̩'),
-                  ('de', 'gewürz', 'ɡəvʏʁt͡s')] * 1000
+if __name__ == "__main__":
+    tsv_files = glob("/home/alex/projects/work_dir/wikipron/data/scrape/tsv/*.tsv")
+    train_data = []
+    for f in tsv_files:
+        language_code = f.split("/")[-1].split("_")[0]
+        with open(
+            f,
+            "r",
+            encoding="utf-8",
+        ) as f:
+            lines = f.readlines()
+        # Prepare data as tuples (lang, word, phoneme)
+        lines = [l.replace(" ", "").replace("\n", "") for l in lines]
+        splits = [l.split("\t") for l in lines]
+        for grapheme, phoneme in splits:
+            if len(grapheme) > 0 and len(phoneme) > 0:
+                train_data.append((language_code, grapheme, phoneme))
 
-    val_data = [('en_us', 'young', 'jʌŋ'),
-                ('de', 'benützten', 'bənʏt͡stn̩')] * 100
+    # split into train and validation
+    train_grouped_by_lang = defaultdict(list)
+    validate_grouped_by_lang = defaultdict(list)
 
-    config_file = 'dp/configs/forward_config.yaml'
+    for lang, word, phoneme in train_data:
+        train_grouped_by_lang[lang].append((word, phoneme))
 
-    preprocess(config_file=config_file,
-               train_data=train_data,
-               val_data=val_data,
-               deduplicate_train_data=False)
+    for lang, data in train_grouped_by_lang.items():
+        random.shuffle(data)
+        n = min(1000, int(len(data) * 0.01))
+        validate_grouped_by_lang[lang].extend(data[:n])
+        train_grouped_by_lang[lang] = data[n:]
+
+    train_data = []
+    validation_data = []
+    for lang, data in train_grouped_by_lang.items():
+        for grapheme, phoneme in data:
+            train_data.append((lang, grapheme, phoneme))
+
+    for lang, data in validate_grouped_by_lang.items():
+        for grapheme, phoneme in data:
+            validation_data.append((lang, grapheme, phoneme))
+
+    config_file = "dp/configs/autoreg_config.yaml"
+
+    preprocess(
+        config_file=config_file,
+        train_data=train_data,
+        val_data=validation_data,
+        deduplicate_train_data=True,
+    )
 
     num_gpus = torch.cuda.device_count()
 
